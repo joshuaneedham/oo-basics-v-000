e5d7d973cc6cd9fe81153ca48dfafb733332716f
Joshua Needham <joshuabneedham@gmail.com>
2017-10-30 16:33:38 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
- 
+
   def initialize(brand)
     @brand = brand
   end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
- 
+
+
+
 end
