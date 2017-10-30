8106e50ee7ef1f41aef707afdff350eb949f9379
Joshua Needham <joshuabneedham@gmail.com>
2017-10-30 16:36:36 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,13 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-
+ 
   def initialize(title)
     @title = title
   end
-
-  def cobble
-    if @condition != new
-      repair
-    else
-      no_repair
-    end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-
+ 
 end
